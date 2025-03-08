93ee68f4082cc65a7903d4d211c6f23332cdebc8
Andrey Surkov <andronsurkov73@gmail.com>
2025-03-08 12:32:02 +0300

    edit background module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -13,11 +13,11 @@ export class BackgroundModule extends Module {
 	}
 
 	getRandomColor() {
-		const letters = '0123456789ABCDEF'
-		let color = '#'
-		for (let i = 0; i < 6; i++) {
-			color += letters[Math.floor(Math.random() * 16)]
-		}
+		const r = Math.floor(Math.random() * 256)
+		const g = Math.floor(Math.random() * 256)
+		const b = Math.floor(Math.random() * 256)
+		const a = Math.random().toFixed(2)
+		const color = `rgba(${r}, ${g}, ${b}, ${a})`
 		return color
 	}
 }
